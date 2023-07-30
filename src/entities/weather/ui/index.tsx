fdd3a70560f14d2fc5fe0f69661cfe548196742e
inu255 <artmvhdv@mail.ru>
2023-07-30 22:04:35 +0700

    api hook

--- src/entities/weather/ui/index.tsx
+++ src/entities/weather/ui/index.tsx
@@ -1,9 +1,23 @@
+import { useGetWeather } from "src/shared/api";
 import MainData from "./main-data";
+import { useEffect } from "react";
 
 export function Weather() {
-  return (
-    <div>
-      <MainData />
-    </div>
-  );
+  const { data, isLoading, isError } = useGetWeather();
+
+  useEffect(() => {
+    if (data && !isLoading && !isError) {
+      console.log(data);
+    }
+  }, [data, isLoading, isError]);
+
+  if (isLoading) {
+    return "Loading...";
+  } else {
+    return (
+      <div>
+        <MainData />
+      </div>
+    );
+  }
 }
