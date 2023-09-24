2b007c9918d6e547d14cfca5e80defc3158be856
inu255 <artmvhdv@mail.ru>
2023-09-24 16:57:33 +0700

    vite build fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import checker from "vite-plugin-checker";
 
-export default defineConfig({
-  base: "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/weather/" : "/",
   plugins: [react(), tsconfigPaths(), checker({ typescript: true, stylelint: false })],
-});
+}));
