572e6c980305845830553d41a211a28bce1a2371
Artyom <artmvhdv@mail.ru>
2023-10-10 10:53:29 +0700

    fix city search

--- src/widgets/sidebar/model.ts
+++ src/widgets/sidebar/model.ts
@@ -2,4 +2,4 @@ import { createEvent, createStore } from "effector";
 
 export const triggerSidebar = createEvent<boolean>();
 
-export const $store = createStore<boolean>(true).on(triggerSidebar, (state) => !state);
+export const $store = createStore<boolean>(false).on(triggerSidebar, (state) => !state);
